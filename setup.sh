1a29e427cc76bead163fdaabe4d2bfa719be5ca9
Amarpreet Kaur <96805692+Amarpreet3@users.noreply.github.com>
2024-04-29 17:28:21 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Exit if any command fails
+set -e
+
+# Step 1: Install python venv module
+echo "Installing python3.11-venv..."
+sudo apt install python3.11-venv
+
+echo "Creating Python virtual environment..."
+python3 -m venv .venv
+
+# Step 2: Install dependencies
+echo "Installing dependencies from requirements.txt..."
+source .venv/bin/activate
+python3 -m pip install -r requirements.txt
+deactivate
+
+# Step 3: Grant access to folders
+echo "Changing permissions for notebooks and data folders..."
+sudo chmod 777 notebooks
+sudo chmod 777 data
+sudo chmod 777 scripts
+
+# Step 4: Spin up docker container
+echo "Starting Docker containers..."
+docker compose up -d
+
+# Step 5: Check Jupyter server
+docker exec spark-notebook jupyter server list
+
+echo "Setup complete!"
